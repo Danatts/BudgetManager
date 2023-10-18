8017845656d7291ccb251e800b5410caee676d27
Daniel Arteta <danielatts97@hotmail.com>
2023-10-18 11:10:02 -0500

    delete: account files

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,7 @@
 pub mod budget;
 pub mod transaction;
-pub mod account;
 pub mod cli;
 
-use crate::account::Account;
-use crate::cli::{CliAction::*, CliArgs};
-use clap::Parser;
-use rusqlite::Connection;
-
 fn main() {
-    let CliArgs { action } = CliArgs::parse();
-
-    let connection = Connection::open_in_memory().expect("could not connect to database");
-
-    account::create_accounts_table(&connection).expect("could not create account's table");
-
-    match action {
-        List => account::list_accounts(&connection).expect("could not list accounts"),
-        Add {
-            amount,
-            entity,
-            category,
-        } => account::add_account(&connection, Account::new(amount, entity, category))
-            .expect("could not add new account"),
-        Update => unimplemented!(),
-        Remove => unimplemented!(),
-    };
-
-    account::list_accounts(&connection).expect("could not list accounts");
+    println!("Hello world!")
 }
