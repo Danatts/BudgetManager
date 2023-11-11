14a6ea54102b4aebe0cdfecbd7578cf9b96c4b99
Daniel Arteta <danielatts97@hotmail.com>
2023-11-11 17:45:43 -0500

    feat: add update query

--- src/database.rs
+++ src/database.rs
@@ -1,3 +1,4 @@
+use crate::budget::Budget;
 use rusqlite::{Connection, Error};
 
 pub fn open_db(path: &str) -> Result<Connection, Error> {
@@ -12,7 +13,38 @@ pub fn create_budget_table(db: &Connection) {
             name TEXT NOT NULL UNIQUE,
             initial_funds REAL NOT NULL,
             current_funds REAL NOT NULL
-        )";
+        );";
     db.execute(query, ())
         .expect("could not create budget table");
+}
+
+pub fn insert_new_budget(db: &Connection, budget: &Budget) {
+    let query = "
+        INSERT INTO budgets (name, initial_funds, current_funds)
+        VALUES (?1, ?2, ?3);";
+    db.execute(
+        query,
+        (&budget.name, &budget.initial_funds, &budget.current_funds),
+    )
+    .expect("could not insert new budget to database");
+}
+
+pub fn update_budget(db: &Connection, budget: &Budget) {
+    let query = "
+        UPDATE budgets
+        SET name = ?1,
+            initial_funds = ?2,
+            current_funds = ?3
+        WHERE
+            budget_id = ?4";
+    db.execute(
+        query,
+        (
+            &budget.name,
+            &budget.initial_funds,
+            &budget.current_funds,
+            &budget.budget_id.unwrap(),
+        ),
+    )
+    .expect("could not update budget in database");
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,24 @@ pub mod budget;
 pub mod database;
 pub mod transaction;
 
+use budget::Budget;
+
 fn main() {
-    let db = database::open_db("./database.db3").expect("could not connect to database");
+    let db = database::open_db("./database.test.db3").expect("could not connect to database");
     database::create_budget_table(&db);
+
+    let mut budget = Budget::new("Mercado", 500.0);
+    database::insert_new_budget(&db, &budget);
+    println!("{:?}", budget);
+    budget.budget_id = Some(1);
+    budget.name = "Libre".to_string();
+    database::update_budget(&db, &budget);
+    println!("{:?}", budget);
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn main_ok() {}
 }
