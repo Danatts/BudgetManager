5df08d6f4a731f6457679fd1097f27f5fdfb2aaf
Daniel Arteta <danielatts97@hotmail.com>
2023-12-19 19:57:41 -0500

    feat: budget names are capitalized

---
+++ src/utils.rs
@@ -0,0 +1,7 @@
+pub fn capitalize(text: &str) -> String {
+    let mut chars = text.chars();
+    match chars.next() {
+        None => String::new(),
+        Some(letter) => letter.to_uppercase().collect::<String>() + chars.as_str(),
+    }
+}
