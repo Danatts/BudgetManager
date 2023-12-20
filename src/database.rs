ec412f2383f4636203d01ac5aad2ae89ba98dde2
Daniel Arteta <danielatts97@hotmail.com>
2023-12-19 23:41:55 -0500

    feat: database located in home dir

--- src/database.rs
+++ src/database.rs
@@ -1,11 +1,28 @@
-use std::process;
+use home;
 use rusqlite::Connection;
+use std::process;
 
-pub fn open_db(path: &str) -> Connection {
+// TODO:
+// 1. el usuario pueda dar una ruta que quiera
+// 2. implementar elección de la base de datos depende del entorno
+pub fn open_default_db() -> Connection {
+    let path = home::home_dir().map(|mut home| {
+        home.push("budget.test.db3");
+        home
+    });
+
+    let path = match path {
+        Some(path) => path,
+        None => {
+            eprintln!("Unable to get home directory");
+            process::exit(1);
+        }
+    };
+
     match Connection::open(path) {
         Ok(db) => db,
         Err(error) => {
-            eprintln!("database connection failed: {}", error);
+            eprintln!("Database connection failed: {}", error);
             process::exit(1);
         }
     }
